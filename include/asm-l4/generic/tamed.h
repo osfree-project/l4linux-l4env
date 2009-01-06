a67bd6370a6d37b63e5705a8a5fb1e2816a58796
l4check <l4check@98c5fe5d-2a70-4002-a3d4-a65c4b1dd206>
2009-01-06 03:50:50 +0000

    update: sync
    git-svn-id: http://svn.tudos.org/repos/l4linux/trunk@134 98c5fe5d-2a70-4002-a3d4-a65c4b1dd206

--- include/asm-l4/generic/tamed.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ASM_L4__GENERIC__TAMED_H__
-#define __ASM_L4__GENERIC__TAMED_H__
-
-void l4x_tamed_init(void);
-void l4x_tamed_set_mapping(int cpu, int nr);
-int  l4x_tamed_print_cli_stats(char *buffer);
-
-#endif /* ! __ASM_L4__GENERIC__TAMED_H__ */
